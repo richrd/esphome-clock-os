aabc95830c2293e4036f66497f062c7c4a08c870
Richard Lewis <richrd@users.noreply.github.com>
2026-01-16 20:49:07 +0200

    Add minimal boot screen.

---
+++ clockos/packages/features/boot_screen.cpp
@@ -0,0 +1,18 @@
+|-
+    // Config
+    int screen_w = it.get_width();
+    int screen_h = it.get_height();
+
+    static int progress = 0;
+    progress += 5;
+
+
+    it.fill(COLOR_OFF);
+    int bar_height = 4;
+    int y = (screen_h - bar_height) / 2;
+    it.rectangle(0, y, screen_w, bar_height);
+    it.filled_rectangle(0, y, progress, bar_height);
+
+    if (progress > screen_w) {
+        id(script_back_to_main_page)->execute();
+    }
